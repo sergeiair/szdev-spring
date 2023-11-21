833a63e0b84c7aa61a87dfaf53f2a66745f7534c
Siarhei Zhardzetski <zsggem@gmail.com>
2023-11-21 13:29:36 +0100

    first commit

---
+++ src/main/java/com/sz/dev/SzDevApplication.java
@@ -0,0 +1,13 @@
+package com.sz.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sz/dev/SzDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sz.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
